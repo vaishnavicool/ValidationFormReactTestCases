82d113c02cd41175f57db8e6dcf6fcd7d4a9d52b
Sethu <Sethu>
2022-06-22 20:14:06 +0530

    fix menubar

--- src/layout/menubar/index.tsx
+++ src/layout/menubar/index.tsx
@@ -4,8 +4,10 @@ import { Dropdown } from "react-bootstrap"
 import { FormattedMessage } from "react-intl"
 import Image from "shared/Image"
 import menuConfig from "./config"
+import { useState } from "react"
 
 const Menubar = () => {
+  const [hover, setHover] = useState(false)
   return (
     <>
       
@@ -22,7 +24,7 @@ const Menubar = () => {
                     className="bg-transparent border-0 custom-dropdown btn btn-light"
                   >
                     <Image src={`${d.icon}_grey.png`} className="menubar-icon"/>
-                    <FormattedMessage id={d.title} />
+                    <span className="menubar-name"><FormattedMessage id={d.title} /></span>
                   </button>
                 </div>
               </div>
@@ -34,7 +36,7 @@ const Menubar = () => {
                     id="dropdown-basic"
                     className="bg-transparent border-0 custom-dropdown"
                   >
-                    <Image src={`${d.icon}_grey.png`} className="menubar-icon"/><span className="menubar-name"><FormattedMessage id={d.title} /></span>
+                    <Image src={`${d.icon}_grey.png`} className="menubar-icon" /><span className="menubar-name"><FormattedMessage id={d.title} /></span>
                   </Dropdown.Toggle>
                   <Dropdown.Menu className="menu">
                     {d.children.map((child) => (
