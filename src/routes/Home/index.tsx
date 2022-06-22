80fdc522c73120fb1266930e68a02a352591f3e2
Sethu <Sethu>
2022-06-22 20:30:10 +0530

    fix layout for filter

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,5 +1,6 @@
 import "./style.scss"
 
+import FilterChipPanel from "./components/FilterChipPanel";
 import TablePanel from "./components/TablePanel";
 import TopSection from "../../layout/breadcrumb/index";
 import useEnhancer from "./enhancer"
@@ -10,7 +11,7 @@ function Dashboard(props: any) {
   return (
     <>
     <TopSection {...extraProps}/>
-    <div className='filterchip-container'>FIlter chips</div>
+    <div className='filterchip-container'><FilterChipPanel /></div>
         
     <div className="container-fluid p-0">
       <div className="profile-container pt-3 ">
