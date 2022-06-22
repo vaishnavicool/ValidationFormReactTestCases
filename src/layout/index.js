4bdbf2f020004ff8b3a79c3308059c8d0e5fea95
Sethu <Sethu>
2022-06-22 14:12:50 +0530

    core layout

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,26 +1,41 @@
-import React from "react"
-import { PreLoad } from "shared/Image"
-import Header from "./header"
-import Footer from "./footer"
-import Routes from "routes"
-import Menubar from "./menubar"
 import './style.scss'
+
+import Footer from "./footer"
+import Header from "./header"
+import Menubar from "./menubar"
+import { PreLoad } from "shared/Image"
+import React from "react"
+import Routes from "routes"
+
 function MainWrapper(props) {
   return (
-    <div>
-      <div className="header container-fluid">
-        <Header />
+    // <div>
+    //   <div className="header container-fluid">
+    //     <Header />
+    //   </div>
+    //   <div className="header container-fluid">
+    //     <Menubar />
+    //   </div>
+    //   <div className="page">
+    //     <Routes />
+    //   </div>
+    //   <div className="footer">
+    //     <Footer />
+    //     <PreLoad />
+    //   </div>
+    // </div>
+    <div className="container-fluid layout">
+      <div className='row'>
+      <div className='col-2 left-container'><Menubar /></div>
+      <div className='col-10 g-0'>
+        <div className='header-container'><Header /></div>
+        <div className='breadcrum-container'>Breadcrumb</div>
+        <div className='filterchip-container'>FIlter chips</div>
+        <div className='table-container'>DatTable</div>
+        <div className='footer-container'><Footer /><PreLoad /></div>
       </div>
-      <div className="header container-fluid">
-        <Menubar />
       </div>
-      <div className="page">
-        <Routes />
-      </div>
-      <div className="footer">
-        <Footer />
-        <PreLoad />
-      </div>
+      
     </div>
   )
 }
--- src/layout/menubar/index.tsx
+++ src/layout/menubar/index.tsx
@@ -1,52 +1,59 @@
-import React, { Fragment } from "react"
 import "./style.scss"
+
 import { Dropdown } from "react-bootstrap"
 import { FormattedMessage } from "react-intl"
+import Image from "shared/Image"
+import React from "react"
 import menuConfig from "./config"
 
-function Menubar(props: any) {
+const Menubar = () => {
   return (
     <>
-      <div className="menubar row">
-        {menuConfig.map((d, index) =>
-          !d.children ? (
-            <div className="col" key={index}>
-              <div className="custom-dropdown">
-                <button
-                  type="button"
-                  id="dropdown-basic"
-                  aria-expanded="false"
-                  className="bg-transparent border-0 custom-dropdown  btn btn-light"
-                >
-                <FormattedMessage id={d.title} />
-
-                </button>
+      <div>
+        <Image className="menu-icon" src="menu-icon.png" alt="sortIcon" />
+        <Image src="Logo.png" alt="coreLogo" className="logo-img" />
+      </div>
+      <div className="menu-list">
+        <div className="menubar">
+          {menuConfig.map((d, index) =>
+            !d.children ? (
+              <div className="col" key={index}>
+                <div className="custom-dropdown">
+                  <button
+                    type="button"
+                    id="dropdown-basic"
+                    aria-expanded="false"
+                    className="bg-transparent border-0 custom-dropdown btn btn-light"
+                  >
+                    <FormattedMessage id={d.title} />
+                  </button>
+                </div>
               </div>
-            </div>
-          ) : (
-            <div className="col">
-              <Dropdown>
-                <Dropdown.Toggle
-                  variant="light"
-                  id="dropdown-basic"
-                  className="bg-transparent border-0 custom-dropdown"
-                >
-                  <FormattedMessage id={d.title} />
-                  
-                </Dropdown.Toggle>
-                <Dropdown.Menu className="menu">
-                  {d.children.map((child) => (
-                    <Dropdown.Item key={child.path} href={child.path}>
-                      <FormattedMessage id={child.title} />
-                    </Dropdown.Item>
-                  ))}
-                </Dropdown.Menu>
-              </Dropdown>
-            </div>
-          )
-        )}
+            ) : (
+              <div className="col">
+                <Dropdown drop="end">
+                  <Dropdown.Toggle
+                    variant="light"
+                    id="dropdown-basic"
+                    className="bg-transparent border-0 custom-dropdown"
+                  >
+                    <FormattedMessage id={d.title} />
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu className="menu">
+                    {d.children.map((child) => (
+                      <Dropdown.Item key={child.path} href={child.path}>
+                        <FormattedMessage id={child.title} />
+                      </Dropdown.Item>
+                    ))}
+                  </Dropdown.Menu>
+                </Dropdown>
+              </div>
+            )
+          )}
+        </div>
       </div>
     </>
   )
 }
+
 export default Menubar
