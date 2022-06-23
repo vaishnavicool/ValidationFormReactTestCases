c9028fa036b76236f49210639b4f8811061d9dea
Sethu <Sethu>
2022-06-23 09:20:23 +0530

    fix

--- src/layout/menubar/config.tsx
+++ src/layout/menubar/config.tsx
@@ -110,58 +110,16 @@ const menuConfig = [
     title: "menubar.reporting",
     path: "/",
     icon:"ic_reporting",
-    children: [
-      {
-        title: "One",
-        path: "/one",
-      },
-      {
-        title: "Two",
-        path: "/two",
-      },
-      {
-        title: "Three",
-        path: "/three",
-      },
-    ],
   },
   {
     title: "menubar.printing",
     path: "/",
     icon:"ic_printing",
-    children: [
-      {
-        title: "One",
-        path: "/one",
-      },
-      {
-        title: "Two",
-        path: "/two",
-      },
-      {
-        title: "Three",
-        path: "/three",
-      },
-    ],
   },
   {
     title: "menubar.manage",
     path: "/",
     icon:"ic_manage",
-    children: [
-      {
-        title: "One",
-        path: "/one",
-      },
-      {
-        title: "Two",
-        path: "/two",
-      },
-      {
-        title: "Three",
-        path: "/three",
-      },
-    ],
   },
 ]
 
