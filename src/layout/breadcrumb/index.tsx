915730721eacd043793c4c3081b792cf5c51f4ae
Sethu <Sethu>
2022-06-30 11:03:41 +0530

    btn-add

--- src/layout/breadcrumb/index.tsx
+++ src/layout/breadcrumb/index.tsx
@@ -1,17 +1,23 @@
-import React, { Fragment } from "react";
-import "./style.scss";
+import React, { Fragment } from "react"
+import "./style.scss"
 import { FormattedMessage } from "react-intl"
 
-
-function TopSection(props:any) {
+function TopSection(props: any) {
   return (
     <Fragment>
       <div className="mainContainer">
-        <p className="content">
-        <FormattedMessage id="breadcrumb" /></p>
+        <div className="content">
+          <FormattedMessage id="breadcrumb" />
+        </div>
+        <div className="button-container">
+          <input
+            className="btn-addworkorder"
+            value="+ Add Workorder"
+            type="button"
+          />
+        </div>
       </div>
-      
     </Fragment>
-  );
+  )
 }
-export default TopSection;
+export default TopSection
