d84e9e6a7ecc3bed2506eed716114db18777c62c
Sethu <Sethu>
2022-07-01 11:11:05 +0530

    columns div

--- src/routes/Home/components/breadcrumb/index.tsx
+++ src/routes/Home/components/breadcrumb/index.tsx
@@ -3,7 +3,7 @@ import "./style.scss"
 import { FormattedMessage } from "react-intl"
 import Image from "shared/Image"
 function TopSection(props: any) {
-  let {setModalType,setShow} = props
+  let { setModalType, setShow, pageOpts } = props
   return (
     <Fragment>
       <div className="mainContainer">
@@ -11,18 +11,37 @@ function TopSection(props: any) {
           <FormattedMessage id="breadcrumb" />
         </div>
         <div className="button-container">
-        <button className="btn btn-grp " type="reset">
-          <Image src="icon-columns.png" className="btn-icon" /> Columns
-        </button>
-        <button className="btn btn-grp " type="reset">
-          <Image src="icon-reload.png" className="btn-icon"/> Reload
-        </button>
-         
+          <button className="btn btn-grp " type="reset">
+            <Image src="icon-columns.png" className="btn-icon" /> Columns
+            <div className="columns-div">
+              {pageOpts.columns.map((d: any) => {
+                return (
+                  <>
+                    {typeof(d.name) !== "object" && (
+                      <div className="column-data">
+                        <span>
+                          <input type="checkbox" />
+                        </span>
+                        <span className="col-name">{d.name}</span>
+                      </div>
+                    )}
+                  </>
+                )
+              })}
+            </div>
+          </button>
+          <button className="btn btn-grp " type="reset">
+            <Image src="icon-reload.png" className="btn-icon" /> Reload
+          </button>
+
           <input
             className="btn-addworkorder"
             value="+ Add Workorder"
             type="button"
-            onClick={() => {setModalType("add");setShow(true)}}
+            onClick={() => {
+              setModalType("add")
+              setShow(true)
+            }}
           />
         </div>
       </div>
