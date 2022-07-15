235b3d035d13d43c9cf592201f775a255144b95b
Sethu <Sethu>
2022-07-15 12:24:09 +0530

    menifest right panel

--- src/routes/AddWorkorder/components/CreateManifest/index.tsx
+++ src/routes/AddWorkorder/components/CreateManifest/index.tsx
@@ -10,12 +10,21 @@ const CreateManifest = () => {
         <Form config={pageConfig?.addManifestConfig} />
       </div>
       <div className="col-6 addmanifeast-container">
-      <div className="rightitem-dots">
-            <Image src="dots.png" />
-          </div>
+        <div className="rightitem-dots">
+          <Image src="dots.png" />
+        </div>
         <div className="rightitem-container">
           <div className="rightitems-text">More Actions</div>
           <div className="rightitem-link">All actions open in new tab</div>
+        </div>
+        <div className="leftitem-container">
+          <div className="left-text">Documents</div>
+          <div className="left-textsmall">
+            What type of files are you uploading?{" "}
+            <span className="icon-info">
+              <Image src="ic_info.png" />
+            </span>
+          </div>
         </div>
       </div>
     </div>
