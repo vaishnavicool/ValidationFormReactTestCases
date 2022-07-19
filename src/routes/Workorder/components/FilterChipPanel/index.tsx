d9183eefeaa5f7bb850f76107df50c3b6b11d14f
Sethu <Sethu>
2022-07-19 17:47:42 +0530

    remove cross icon if no filter

--- src/routes/Workorder/components/FilterChipPanel/index.tsx
+++ src/routes/Workorder/components/FilterChipPanel/index.tsx
@@ -6,32 +6,34 @@ import EllipsisText from "react-ellipsis-text"
 
 const FilterChipPanel = (props) => {
   let { setShow, setModalType, aFilters, removeAFilter, clearFilters } = props
+  console.log(aFilters)
   return (
     <>
       <div className="filter-input">
         <div className="d-flex ellipsis">
-          {Object.keys(aFilters)         
-           .sort((a, b) => a.localeCompare(b))          
-          .map((d) => (
-            <span key={d} className=
-            "badge filter-chip">          
-              <FormattedMessage id={`form.${d}`} />:
-              <EllipsisText text={aFilters[d]} length={20} />             
-              <Image
-                src="cross.png"
-                className="filter-remove"
-                onClick={() => removeAFilter(d)}
-              />
-            </span>
-          ))}
+          {Object.keys(aFilters)
+            .sort((a, b) => a.localeCompare(b))
+            .map((d) => (
+              <span key={d} className="badge filter-chip">
+                <FormattedMessage id={`form.${d}`} />:
+                <EllipsisText text={aFilters[d]} length={20} />
+                <Image
+                  src="cross.png"
+                  className="filter-remove"
+                  onClick={() => removeAFilter(d)}
+                />
+              </span>
+            ))}
         </div>
-        <div className="clear-filter">
-          <Image
-            src="icon-cross.png"
-            style={{ cursor: "pointer" }}
-            onClick={clearFilters}
-          />
-        </div>
+        {Object.keys(aFilters).length > 0 && (
+          <div className="clear-filter">
+            <Image
+              src="icon-cross.png"
+              style={{ cursor: "pointer" }}
+              onClick={clearFilters}
+            />
+          </div>
+        )}
       </div>
       <Button
         variant="link"
