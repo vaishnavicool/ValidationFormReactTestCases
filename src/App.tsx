083253d0b1d871a51766e4e4423a74f06c1ede85
Pavni <pavani.sahni@rubrik.com>
2022-09-15 11:41:52 +0530

    sdd/feature delivery option route added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Counter } from "./Pages/Counter/Counter";
 import { Posts } from "./Pages/Posts/Posts";
+import { DeliveryOptions } from './Pages/sdd/DeliveryOptions/DeliveryOptions'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Counter />} />
         <Route path="/posts" element={<Posts />} />
+        <Route path="/delivery" element={<DeliveryOptions />} />
       </Routes>
     </BrowserRouter>
   );
--- src/Pages/sdd/DeliveryOptions/DeliveryOptions.tsx
+++ src/Pages/sdd/DeliveryOptions/DeliveryOptions.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 export const DeliveryOptions = () => {
 
@@ -8,11 +8,11 @@ export const DeliveryOptions = () => {
 
   return (
     <>
-    <form class="form__radio-group">
+    <form className="form__radio-group">
       <fieldset>
         <legend className="form__radio-legend">Shipping and delivery options</legend>
         <input type="radio" name="form__stm" id="form__shiptome" className="form__radio-input" aria-describedby="Arrives in 5-6 business days"/>
-        <label class="form__label-radio" htmlFor="form__shiptome" className="form__radio-label" />
+        <label htmlFor="form__shiptome" className="form__radio-label" />
       </fieldset>
     </form>
     </>
